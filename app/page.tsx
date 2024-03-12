1671f746ebf4526794ca25bb06164f5a3224e173
Matheus Sousa <devlpsousa@gmail.com>
2024-03-12 12:20:46 -0300

    feat: ensure User Context is updated when onAuthStageChanged

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,31 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router.js";
 import { onAuthStateChanged } from "firebase/auth";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 // Utilities
-import { auth } from "./config/firebase.config";
+import { auth, db } from "./config/firebase.config";
+import { UserContext } from "./contexts/user.context";
+import { useContext } from "react";
 
 export default function Page() {
-  onAuthStateChanged(auth, (user) => {
-    console.log(user);
+  const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext);
+
+  onAuthStateChanged(auth, async (user) => {
+    const isSigningOut = isAuthenticated && !user;
+    if (isSigningOut) {
+      return logoutUser();
+    }
+
+    const inSigningIn = !isAuthenticated && user;
+    if (inSigningIn) {
+      const querySnapshot = await getDocs(query(collection(db, "users"), where("id", "==", user.uid)));
+
+      const userFromFirestore = querySnapshot.docs[0]?.data();
+
+      return loginUser(userFromFirestore as any);
+    }
   });
+
   return <RouterProvider router={router} />;
 }
