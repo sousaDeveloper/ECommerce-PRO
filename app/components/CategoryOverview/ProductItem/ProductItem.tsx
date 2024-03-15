ea0691c6d43f68ba61359df29dc97de490c64e75
Matheus Sousa <devlpsousa@gmail.com>
2024-03-15 13:02:33 -0300

    chore: change in boxShadow and hover at button

--- app/components/CategoryOverview/ProductItem/ProductItem.tsx
+++ app/components/CategoryOverview/ProductItem/ProductItem.tsx
@@ -24,7 +24,13 @@ export default function ProductItem({ product }: IProductProps) {
   }, []);
 
   return (
-    <div className="max-w-[20rem] rounded bg-[#8C3A60] bg-clip-border text-white shadow-md" key={product.id}>
+    <div
+      className="max-w-[20rem] rounded bg-[#8C3A60] bg-clip-border text-white"
+      style={{
+        boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+      }}
+      key={product.id}
+    >
       <div className="mx-4 -mt-6 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white">
         <Image
           src={product.imageUrl}
@@ -43,7 +49,7 @@ export default function ProductItem({ product }: IProductProps) {
       </div>
       <div className="flex justify-between items-center p-6 pt-0">
         <span className="font-bold">R${product.price}</span>
-        <button className="bg-[#fff] hover:bg-[#283040] hover:text-white transition duration-300 text-[#283040] rounded p-2 font-bold">
+        <button className="bg-[#fff] hover:bg-[#283040] hover:text-[#F2B6C1] transition duration-300 text-[#283040] rounded p-2 font-bold">
           Adicionar ao carrinho
         </button>
       </div>
