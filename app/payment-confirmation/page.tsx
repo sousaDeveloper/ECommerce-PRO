ac8b6d5c079d3aee101a6ae61c1cabe1ef18028d
Matheus Sousa <devlpsousa@gmail.com>
2024-03-22 14:59:24 -0300

    feat: add Page payment-confirmed of Stripe

---
+++ app/payment-confirmation/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useContext, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+// Utilities
+import { CartContext } from "../contexts/cart.context";
+
+// Components
+import Header from "../components/Header/Header";
+import PaymentConfirmed from "../components/PaymentConfirmed/PaymentConfirmed";
+
+export default function PaymentConfirmation() {
+  const router = useRouter();
+  const { clearCart } = useContext(CartContext);
+  const searchParams = useSearchParams();
+
+  const status = searchParams.get("success");
+
+  useEffect(() => {
+    if (status === "true") {
+      setTimeout(() => {
+        router.push("/");
+        return clearCart();
+      }, 3000);
+    }
+  }, [searchParams]);
+
+  return (
+    <>
+      <Header />
+      <div className="grid place-content-center place-items-center">{status === "true" && <PaymentConfirmed />}</div>
+      <h1>{status === "false" && "Compra Negada!"}</h1>
+    </>
+  );
+}
