9af45fac16e453b57318f77db9878a912738737e
Matheus Sousa <devlpsousa@gmail.com>
2024-03-23 11:25:38 -0300

    feat: add useParams of Next

---
+++ app/pages/category/[id]/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+// Components
+import CategoryDetails from "../../../components/CategoriesDetails/CategoriesDetails";
+import Header from "../../../components/Header/Header";
+
+export default function CategoryDetailsPage() {
+  const { id } = useParams();
+
+  if (!id) return null;
+
+  return (
+    <>
+      <Header />
+      <CategoryDetails categoryId={`${id}`} />
+    </>
+  );
+}
