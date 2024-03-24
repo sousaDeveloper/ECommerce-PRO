bad005fe4ed3d3910daa69b7519364b2d778a5ae
Matheus Sousa <devlpsousa@gmail.com>
2024-03-24 11:31:32 -0300

    chore: changing skeleton loading

--- app/components/SkeletonLoading/SkeletonLoading.tsx
+++ app/components/SkeletonLoading/SkeletonLoading.tsx
@@ -1,12 +1,13 @@
+import "./SkeletonLoading.scss";
+
 export default function SkeletonLoading() {
   return (
-    <div className="w-[20rem] h-[32rem] animate-pulse rounded p-4 bg-[#8C3A60] bg-clip-border text-white">
-      <div className="bg-neutral-400/50 w-full animate-pulse rounded-md"></div>
-      <div className="flex flex-col gap-2 ">
-        <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-        <div className="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
-        <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-        <div className="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
+    <div className="card h-[30rem] rounded">
+      <div className="card__skeleton card__description -mt-10"> </div>
+      <div className="card__skeleton card__title w-[80%]"></div>
+      <div className="flex items-center justify-between mt-6 rounded">
+        <div className="card__skeleton card-price w-[20%]"></div>
+        <div className="card__skeleton card-button px-16 py-4"></div>
       </div>
     </div>
   );
