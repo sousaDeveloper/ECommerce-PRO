9ceeeccd5022a06f36174a7b59f54a8cf9122824
Matheus Sousa <devlpsousa@gmail.com>
2024-03-26 13:54:18 -0300

    chore: create folder components in homePage

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -1,8 +1,9 @@
-import Categories from "@components/Categories/Categories";
 import Header from "@components/Header/Header";
-import Carousel from "@components/Carousel/Carousel";
-import SlideContent from "@components/SlideContent/SlideContent";
-import ProductsWithDiscountOverview from "@componentsProductsWithDiscountOverview/ProductsWithDiscountOverview";
+import Carousel from "./components/Carousel/Carousel";
+import SlideContent from "./components/SlideContent/SlideContent";
+import Categories from "@components/Categories/Categories";
+import ProductsWithDiscountOverview from "./components/ProductsWithDiscountOverview/ProductsWithDiscountOverview";
+import Footer from "../../components/Footer/Footer";
 
 export default function HomePage() {
   return (
@@ -12,6 +13,7 @@ export default function HomePage() {
       <SlideContent />
       <Categories />
       <ProductsWithDiscountOverview />
+      <Footer />
     </main>
   );
 }
