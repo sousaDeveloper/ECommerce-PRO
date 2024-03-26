70a321342c17574e9bbdf39328b5bb4a53ba1779
Matheus Sousa <devlpsousa@gmail.com>
2024-03-26 11:04:33 -0300

    chore: remove border in texts and add letter-spacing

--- app/components/SlideContent/SlideContent.tsx
+++ app/components/SlideContent/SlideContent.tsx
@@ -9,7 +9,7 @@ export default function SlideContent() {
   return (
     <>
       {isLargeScreen ? (
-        <h1 className="bg-[#283040] p-2 text-2xl text-[#8C3A60] font-bold">
+        <h1 className="bg-[#283040] p-2 text-2xl text-[#8C3A60] font-bold mt-2">
           <span>
             <span>Seja bem-vindo à Next Store</span>
             <span>Entrega grátis acima de R$500,00</span>
@@ -20,20 +20,12 @@ export default function SlideContent() {
           </span>
         </h1>
       ) : (
-        <div className="slide-container bg-[#283040] p-2">
-          <div className="contentSlide flex gap-20 text-[#8C3A60]">
-            <h1 className="slide-text font-bold text-2xl border border-solid border-[#f2b6c1] rounded px-2">
-              Seja bem-vindo à Next Store
-            </h1>
-            <h1 className="slide-text font-bold text-2xl border border-solid border-[#f2b6c1] rounded px-2">
-              Entrega grátis acima de R$500,00
-            </h1>
-            <h1 className="slide-text font-bold text-2xl border border-solid border-[#f2b6c1] rounded px-2">
-              Entregamos para todo Brasil
-            </h1>
-            <h1 className="slide-text font-bold text-2xl border border-solid border-[#f2b6c1] rounded px-2">
-              Ganhe +20% OFF na primeira compra com o cupom PRIMEIRA20
-            </h1>
+        <div className="slide-container bg-[#283040] p-2 mt-3">
+          <div className="contentSlide flex gap-32 text-[#8C3A60]">
+            <h1 className="slide-text font-bold text-2xl">Seja bem-vindo à Next Store</h1>
+            <h1 className="slide-text font-bold text-2xl">Entrega grátis acima de R$500,00</h1>
+            <h1 className="slide-text font-bold text-2xl">Entregamos para todo Brasil</h1>
+            <h1 className="slide-text font-bold text-2xl">Ganhe +20% OFF na primeira compra com o cupom PRIMEIRA20</h1>
           </div>
         </div>
       )}
