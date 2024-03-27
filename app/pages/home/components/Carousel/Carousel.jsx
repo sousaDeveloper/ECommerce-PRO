b34f43d59f29c36bbe63e59f27f24cd5ba43c79a
Matheus Sousa <devlpsousa@gmail.com>
2024-03-27 08:48:26 -0300

    feat: add swiper navigation

--- app/pages/home/components/Carousel/Carousel.jsx
+++ app/pages/home/components/Carousel/Carousel.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "./Carousel.scss";
 
 // import required modules
