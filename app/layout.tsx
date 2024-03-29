4aeebac071a33dc478c676f4bea5719ae1acbe8c
Matheus Sousa <devlpsousa@gmail.com>
2024-03-29 09:21:32 -0300

    chore: changing font

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,14 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link rel="shortcut icon" href="/favicon_ns_by_nuhamaulana_da5acc2-300w.png" type="image/x-icon" />
         <meta httpEquiv="Cross-Origin-Opener-Policy" content="same-origin" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:ital@0;1&family=Raleway:ital@0;1&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;1,300&display=swap"
           rel="stylesheet"
         />
       </head>
